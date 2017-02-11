0c9dc696a7be34ae5c133b159be6a81d8b67f0e5
Jue Wang's Github <freeleons@gmail.com>
2017-02-12 00:46:13 +0800

    Added new file camera_calibration.py

---
+++ camera_calibration.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('../camera_cal/calibration*.jpg')
+
+# Step through the list and search for chessboard corners
+for fname in images:
+    img = cv2.imread(fname)
+    print(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+img = cv2.imread("camera_cal/calibration1.jpg")
+# Get the image shape/size
+img_size = (img.shape[0], img.shape[1])
+print('image size:', img_size)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# Do camera calibration given object points and image points
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+# ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape, None, None)
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+cv2.imwrite('output_images/undistorted_image.jpg',dst)
+
+# Visualize undistortion
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=30)
+ax2.imshow(dst)
+ax2.set_title('Undistorted Image', fontsize=30)
